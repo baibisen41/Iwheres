86073f8c1ef20d9717a487f87e6aca57f942b2d8
baibisen <m15840972545@163.com>
2016-10-23 11:17:02 +0800

    项目结构

---
+++ BBS/app/src/main/java/com/iwhere/bbs/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iwhere.bbs.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.iwhere.bbs.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
