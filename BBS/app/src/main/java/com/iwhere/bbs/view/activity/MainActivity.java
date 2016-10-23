9a6d6c8cdd5b9f3a1ddbf8bd780bcb7bbafa260f
baibisen <m15840972545@qq.com>
2016-10-23 16:43:34 +0800

    标题栏

--- BBS/app/src/main/java/com/iwhere/bbs/view/activity/MainActivity.java
+++ BBS/app/src/main/java/com/iwhere/bbs/view/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.iwhere.bbs.view.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.iwhere.bbs.R;
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
     }
 }
