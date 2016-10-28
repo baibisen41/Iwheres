77ff503c6a2abac84267280eae4c3613dd601bfd
baibisen <m15840972545@qq.com>
2016-10-28 17:04:49 +0800

    添加侧拉栏布局

--- BBS/app/src/main/java/com/iwhere/bbs/view/activity/MainActivity.java
+++ BBS/app/src/main/java/com/iwhere/bbs/view/activity/MainActivity.java
@@ -1,18 +1,59 @@
 package com.iwhere.bbs.view.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.iwhere.bbs.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity implements View.OnClickListener {
+
+    private ImageView titleLeftBt;
+    private ImageView titleRightBt;
+    protected SlidingMenu leftSlidingMenu;
+    protected SlidingMenu rightSlidingMenu;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        initLeftSlidingMenu();
+        initRightSlidingMenu();
         setContentView(R.layout.activity_main);
+        initView();
+
+
+    }
+
+    private void initView() {
+        titleLeftBt = (ImageView) findViewById(R.id.leftbutton);
+        titleLeftBt.setOnClickListener(this);
+        titleRightBt = (ImageView) findViewById(R.id.rightbutton);
+        titleRightBt.setOnClickListener(this);
+    }
+
+    private void initRightSlidingMenu() {
+
+    }
+
+    private void initLeftSlidingMenu() {
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.leftbutton:
+                leftSlidingMenu.showMenu(true);
+                break;
+            case R.id.rightbutton:
+                rightSlidingMenu.showMenu(true);
+                break;
+            default:
+                break;
+        }
 
     }
 }
