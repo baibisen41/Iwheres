c8b0710fa6c42417cc8065c97e0cb5c0eb7619d1
beasley <m15840972545@163.com>
2016-11-29 17:35:27 +0800

    补充

---
+++ BBS/app/src/main/java/com/iwhere/bbs/controller/SettingContoller.java
@@ -0,0 +1,8 @@
+package com.iwhere.bbs.controller;
+
+/**
+ * Created by beasley on 2016/11/29.
+ */
+
+public class SettingContoller {
+}
---
+++ BBS/app/src/main/java/com/iwhere/bbs/service/SettingService.java
@@ -0,0 +1,8 @@
+package com.iwhere.bbs.service;
+
+/**
+ * Created by beasley on 2016/11/29.
+ */
+
+public class SettingService {
+}
