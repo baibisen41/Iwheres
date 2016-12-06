ff4778f8391d5d03a6abdf69cdbc391f60f92774
beasley <m15840972545@163.com>
2016-12-06 20:12:03 +0800

    修改定位布局

--- BBS/app/src/main/java/com/iwhere/bbs/view/fragment/LocationopenFragment.java
+++ BBS/app/src/main/java/com/iwhere/bbs/view/fragment/LocationopenFragment.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
 import com.iwhere.bbs.R;
 
 /**
@@ -15,14 +18,58 @@ import com.iwhere.bbs.R;
 
 public class LocationopenFragment extends Fragment {
 
+    private View view;
+    private MapView mapView = null;
+    private BaiduMap baiduMap = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+ //       SDKInitializer.initialize(getActivity().getApplicationContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.content_left_location_open, container, false);
+        initView();
+        initData();
         return view;
+    }
+
+    private View initView() {
+        return view;
+    }
+
+    private void initData() {
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 }
