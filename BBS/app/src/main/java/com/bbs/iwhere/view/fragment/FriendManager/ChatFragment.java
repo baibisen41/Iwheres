f85dcf3b000ca2820bb85b2492a529ecb56d42b3
baibisen <m15840972545@163.com>
2017-05-09 08:06:43 +0800

    好友

--- BBS/app/src/main/java/com/bbs/iwhere/view/fragment/FriendManager/ChatFragment.java
+++ BBS/app/src/main/java/com/bbs/iwhere/view/fragment/FriendManager/ChatFragment.java
@@ -1,13 +1,245 @@
 package com.bbs.iwhere.view.fragment.FriendManager;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+import com.bbs.iwhere.view.activity.MainActivity;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMCmdMessageBody;
+import com.hyphenate.chat.EMGroup;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMTextMessageBody;
+import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.hyphenate.easeui.widget.chatrow.EaseChatRow;
+import com.hyphenate.easeui.widget.chatrow.EaseCustomChatRowProvider;
+import com.hyphenate.easeui.widget.emojicon.EaseEmojiconMenu;
+import com.hyphenate.util.EasyUtils;
+import com.hyphenate.util.PathUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
 
 /**
  * Created by 大森 on 2017/4/26.
  */
 
-public class ChatFragment extends EaseChatFragment {
+public class ChatFragment extends EaseChatFragment implements EaseChatFragment.EaseChatFragmentHelper {
+    // constant start from 11 to avoid conflict with constant in base class
+    private static final int ITEM_VIDEO = 11;
+    private static final int ITEM_FILE = 12;
+    private static final int ITEM_VOICE_CALL = 13;
+    private static final int ITEM_VIDEO_CALL = 14;
+
+    private static final int REQUEST_CODE_SELECT_VIDEO = 11;
+    private static final int REQUEST_CODE_SELECT_FILE = 12;
+    private static final int REQUEST_CODE_GROUP_DETAIL = 13;
+    private static final int REQUEST_CODE_CONTEXT_MENU = 14;
+    private static final int REQUEST_CODE_SELECT_AT_USER = 15;
 
 
+    private static final int MESSAGE_TYPE_SENT_VOICE_CALL = 1;
+    private static final int MESSAGE_TYPE_RECV_VOICE_CALL = 2;
+    private static final int MESSAGE_TYPE_SENT_VIDEO_CALL = 3;
+    private static final int MESSAGE_TYPE_RECV_VIDEO_CALL = 4;
+
+    //red packet code : 红包功能使用的常量
+    private static final int MESSAGE_TYPE_RECV_RED_PACKET = 5;
+    private static final int MESSAGE_TYPE_SEND_RED_PACKET = 6;
+    private static final int MESSAGE_TYPE_SEND_RED_PACKET_ACK = 7;
+    private static final int MESSAGE_TYPE_RECV_RED_PACKET_ACK = 8;
+    private static final int MESSAGE_TYPE_RECV_TRANSFER_PACKET = 9;
+    private static final int MESSAGE_TYPE_SEND_TRANSFER_PACKET = 10;
+    private static final int MESSAGE_TYPE_RECV_RANDOM = 11;
+    private static final int MESSAGE_TYPE_SEND_RANDOM = 12;
+    private static final int REQUEST_CODE_SEND_RED_PACKET = 16;
+    private static final int ITEM_RED_PACKET = 16;
+    private static final int REQUEST_CODE_SEND_TRANSFER_PACKET = 17;
+    private static final int ITEM_TRANSFER_PACKET = 17;
+    //end of red packet code
+
+    /**
+     * if it is chatBot
+     */
+    private boolean isRobot;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    protected void setUpView() {
+        setChatFragmentListener(this);
+
+        super.setUpView();
+        // set click listener
+        titleBar.setLeftLayoutClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (EasyUtils.isSingleActivity(getActivity())) {
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                }
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    protected void registerExtendMenuItem() {
+        //use the menu in base class
+        super.registerExtendMenuItem();
+        //extend menu items
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+
+
+                case REQUEST_CODE_SELECT_AT_USER:
+                    if (data != null) {
+                        String username = data.getStringExtra("username");
+                        inputAtUsername(username, false);
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    @Override
+    public void onSetMessageAttributes(EMMessage message) {
+
+    }
+
+    @Override
+    public EaseCustomChatRowProvider onSetCustomChatRowProvider() {
+        return new CustomChatRowProvider();
+    }
+
+
+    @Override
+    public void onEnterToChatDetails() {
+
+    }
+
+    @Override
+    public void onAvatarClick(String username) {
+/*        //handling when user click avatar
+        Intent intent = new Intent(getActivity(), UserProfileActivity.class);
+        intent.putExtra("username", username);
+        startActivity(intent);*/
+    }
+
+    @Override
+    public void onAvatarLongClick(String username) {
+        inputAtUsername(username);
+    }
+
+
+    @Override
+    public boolean onMessageBubbleClick(EMMessage message) {
+
+        return false;
+    }
+
+    @Override
+    public void onCmdMessageReceived(List<EMMessage> messages) {
+
+        super.onCmdMessageReceived(messages);
+    }
+
+    @Override
+    public void onMessageBubbleLongClick(EMMessage message) {
+
+    }
+
+    @Override
+    public boolean onExtendMenuItemClick(int itemId, View view) {
+
+        //keep exist extend menu
+        return false;
+    }
+
+    /**
+     * select file
+     */
+    protected void selectFileFromLocal() {
+        Intent intent = null;
+        if (Build.VERSION.SDK_INT < 19) { //api 19 and later, we can't use this way, demo just select from images
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        } else {
+            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }
+        startActivityForResult(intent, REQUEST_CODE_SELECT_FILE);
+    }
+
+    /**
+     * make a voice call
+     */
+    protected void startVoiceCall() {
+
+    }
+
+    /**
+     * make a video call
+     */
+    protected void startVideoCall() {
+
+    }
+
+    /**
+     * chat row provider
+     */
+    private final class CustomChatRowProvider implements EaseCustomChatRowProvider {
+        @Override
+        public int getCustomChatRowTypeCount() {
+            //here the number is the message type in EMMessage::Type
+            //which is used to count the number of different chat row
+            return 12;
+        }
+
+        @Override
+        public int getCustomChatRowType(EMMessage message) {
+            return 0;
+        }
+
+        @Override
+        public EaseChatRow getCustomChatRow(EMMessage message, int position, BaseAdapter adapter) {
+            if (message.getType() == EMMessage.Type.TXT) {
+
+            }
+            return null;
+        }
+
+    }
 
 }
--- BBS/app/src/main/java/com/bbs/iwhere/view/fragment/FriendManager/ConversationListFragment.java
+++ BBS/app/src/main/java/com/bbs/iwhere/view/fragment/FriendManager/ConversationListFragment.java
@@ -1,25 +1,148 @@
 package com.bbs.iwhere.view.fragment.FriendManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bbs.iwhere.R;
+import com.bbs.iwhere.view.activity.ChatActivity;
+import com.bbs.iwhere.view.activity.MainActivity;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.easeui.model.EaseAtMessageHelper;
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
+import com.hyphenate.easeui.widget.EaseConversationList;
+import com.hyphenate.util.NetUtils;
 
 /**
  * Created by 大森 on 2017/3/29.
  */
 
-public class ConversationListFragment extends Fragment {
+public class ConversationListFragment extends EaseConversationListFragment {
 
-    @Nullable
+    private TextView errorText;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.ab, container, false);
-        return view;
+    protected void initView() {
+        super.initView();
+        hideTitleBar();
+        View errorView = (LinearLayout) View.inflate(getActivity(), R.layout.em_chat_neterror_item, null);
+        errorItemContainer.addView(errorView);
+        errorText = (TextView) errorView.findViewById(R.id.tv_connect_errormsg);
+    }
+
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+        // register context menu
+        registerForContextMenu(conversationListView);
+        conversationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EMConversation conversation = conversationListView.getItem(position);
+                String username = conversation.conversationId();
+                if (username.equals(EMClient.getInstance().getCurrentUser()))
+                    Toast.makeText(getActivity(), R.string.Cant_chat_with_yourself, Toast.LENGTH_SHORT).show();
+                else {
+                    // start chat acitivity
+                    Intent intent = new Intent(getActivity(), ChatActivity.class);
+                    intent.putExtra(EaseConstant.EXTRA_USER_ID, username);
+                    startActivity(intent);
+                }
+            }
+        });
+/*        //red packet code : 红包回执消息在会话列表最后一条消息的展示
+        conversationListView.setConversationListHelper(new EaseConversationList.EaseConversationListHelper() {
+            @Override
+            public String onSetItemSecondaryText(EMMessage lastMessage) {
+                if (lastMessage.getBooleanAttribute(RPConstant.MESSAGE_ATTR_IS_RED_PACKET_ACK_MESSAGE, false)) {
+                    String sendNick = lastMessage.getStringAttribute(RPConstant.EXTRA_RED_PACKET_SENDER_NAME, "");
+                    String receiveNick = lastMessage.getStringAttribute(RPConstant.EXTRA_RED_PACKET_RECEIVER_NAME, "");
+                    String msg;
+                    if (lastMessage.direct() == EMMessage.Direct.RECEIVE) {
+                        msg = String.format(getResources().getString(R.string.msg_someone_take_red_packet), receiveNick);
+                    } else {
+                        if (sendNick.equals(receiveNick)) {
+                            msg = getResources().getString(R.string.msg_take_red_packet);
+                        } else {
+                            msg = String.format(getResources().getString(R.string.msg_take_someone_red_packet), sendNick);
+                        }
+                    }
+                    return msg;
+                } else if (lastMessage.getBooleanAttribute(RPConstant.MESSAGE_ATTR_IS_TRANSFER_PACKET_MESSAGE, false)) {
+                    String transferAmount = lastMessage.getStringAttribute(RPConstant.EXTRA_TRANSFER_AMOUNT, "");
+                    String msg;
+                    if (lastMessage.direct() == EMMessage.Direct.RECEIVE) {
+                        msg = String.format(getResources().getString(R.string.msg_transfer_to_you), transferAmount);
+                    } else {
+                        msg = String.format(getResources().getString(R.string.msg_transfer_from_you), transferAmount);
+                    }
+                    return msg;
+                }
+                return null;
+            }
+        });*/
+        super.setUpView();
+        //end of red packet code
+    }
+
+    @Override
+    protected void onConnectionDisconnected() {
+        super.onConnectionDisconnected();
+        if (NetUtils.hasNetwork(getActivity())) {
+            errorText.setText(R.string.can_not_connect_chat_server_connection);
+        } else {
+            errorText.setText(R.string.the_current_network);
+        }
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getActivity().getMenuInflater().inflate(R.menu.em_delete_message, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        boolean deleteMessage = false;
+        if (item.getItemId() == R.id.delete_message) {
+            deleteMessage = true;
+        } else if (item.getItemId() == R.id.delete_conversation) {
+            deleteMessage = false;
+        }
+        EMConversation tobeDeleteCons = conversationListView.getItem(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).position);
+        if (tobeDeleteCons == null) {
+            return true;
+        }
+        if (tobeDeleteCons.getType() == EMConversation.EMConversationType.GroupChat) {
+            EaseAtMessageHelper.get().removeAtMeGroup(tobeDeleteCons.conversationId());
+        }
+        try {
+            // delete conversation
+            EMClient.getInstance().chatManager().deleteConversation(tobeDeleteCons.conversationId(), deleteMessage);
+ /*           InviteMessgeDao inviteMessgeDao = new InviteMessgeDao(getActivity());
+            inviteMessgeDao.deleteMessage(tobeDeleteCons.conversationId());*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        refresh();
+
+        // update unread count
+//        ((MainActivity) getActivity()).updateUnreadLabel();
+        return true;
     }
 
 }
